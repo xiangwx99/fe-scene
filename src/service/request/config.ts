49e6cbb912852d4294b62a6ac4dbf44b037de5bd
lixiang <lixiang@jwzg.com>
2022-02-22 18:04:00 +0800

    test: test

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -9,9 +9,9 @@ const TIME_OUT = 10000
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = 'http://123.207.32.32:8000'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://coderwhy.org/prod'
+  BASE_URL = 'http://coder.org/prod'
 } else {
-  BASE_URL = 'http://coderwhy.org/test'
+  BASE_URL = 'http://coder.org/test'
 }
 
 export { BASE_URL, TIME_OUT }
